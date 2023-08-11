8e9a4fd3fd0c0426753b48b88d3fdba22d2e21ee
KaphAein <cherifafadaly@gmail.com>
2023-08-11 07:06:21 -0700

    9 var c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,22 @@ int j;
 i = j = 48;
 while (i <  57)
 {
-putchar(i);
-while (j <= 57)
+while (j < 57)
 {
 j++;
+putchar(i);
 putchar(j);
+if (i != 56)
+{
+putchar(44);
+putchar(32);
+i++
 }
-i++;
+else
+{
+i++
+}
+}
 j = i;
 }
 putchar('\n');
