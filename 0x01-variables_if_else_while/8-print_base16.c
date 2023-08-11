ee3380a7dee5ac7d55c5eeba51c5a84aeaff40c4
KaphAein <cherifafadaly@gmail.com>
2023-08-11 05:39:05 -0700

    7 var c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char hxdc;
+hxdc = '0';
+
+while (alphabet <= '9')
+{
+putchar(hxdc);
+hxdc++;
+}
+
+hxdc =  'a';
+
+while (hxdc <= 'f')
+{
+putchar(hxdc);
+hxdc++;
+}
+putchar('\n');
+return (0);
+}
