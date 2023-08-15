fa6f9e6e83d83184eb95670f03cfa88cf0ebead2
KaphAein <cherifafadaly@gmail.com>
2023-08-15 11:06:31 -0700

    8 nes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,20 +10,24 @@ void jack_bauer(void)
 {
 	int i;
 	int j;
+	int firsthour;
+	int seconhour;
+	int firstmin;
+	int secondmin;
 
 	i = 0;
 	while (i < 24)
 	{
 		j = 0;
-		int firsthour = i / 10;
-		int seconhour = i % 10:
+		firsthour = i / 10;
+		seconhour = i % 10;
 		_putchar(firstmin + '0');
 		_putchar(secondmin + '0');
 		_putchar(':');
 		while (j < 60)
 		{
-			int firstmin = j / 10;
-			int secondmin = j % 10:
+			firstmin = j / 10;
+			secondmin = j % 10:
 			_putchar(firstmin + '0');
 			_putchar(secondmin + '0');
 			_putchar('\n');
