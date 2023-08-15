c1bda1b3337df534e7c3c1c2a57777151ffb1ae5
KaphAein <cherifafadaly@gmail.com>
2023-08-15 16:06:39 -0700

    to nth

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,31 +11,33 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	while (n <= 98)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-		if (n != 98)
+		while (n <= 98)
 		{
-			_putchar(',');
-			_putchar(' ');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
 		}
-		n++;
-	}
+		_putchar('\n');
 	}
 	else
 	{
-	while (n >= 98)
-	{
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-		if (n != 98)
+		while (n >= 98)
 		{
-			_putchar(',');
-			_putchar(' ');
+			_putchar(n / 100 + '0');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
 		}
-		n--;
-	}
+		_putchar('\n');
 	}
 }
