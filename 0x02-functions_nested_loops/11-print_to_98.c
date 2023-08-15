74df459c0a0f85a19b7251199ba7316c2a14ea0c
KaphAein <cherifafadaly@gmail.com>
2023-08-15 16:39:28 -0700

    to nth

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,17 +13,15 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = n; i < 98, i++)
+		for (i = n; i < 98; i++)
 		{
 			printf("%d", n);
 			puts(", ");
 		}
 	}
-	int i;
-
 	if (n > 98)
     {
-        for (i = n; i > 98, i--)
+        for (i = n; i > 98; i--)
 		{
 			printf("%d", n);
 			puts(", ");
