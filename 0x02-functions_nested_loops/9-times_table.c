7d5181bd71d6ec95251907f692cad4957d9fe4cd
KaphAein <cherifafadaly@gmail.com>
2023-08-15 14:58:48 -0700

    9 nr

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,16 +13,16 @@ void times_table(void)
 	for(i = 0; i <= 9; i++)
 	{
 		_putchar('0');
-		for(j = 0, j <= 9, j++)
+		for(j = 0; j <= 9; j++)
 		{
 			_putchar(',');
             _putchar(' ');
 			mult = i * j;
-			if (result < 10)
+			if (mult < 10)
 				_putchar(' ');
 			else
-				_putchar(result / 10 + '0');
-			_putchar(result % 10 + '0');
+				_putchar(mult / 10 + '0');
+			_putchar(mult % 10 + '0');
         }
 
         _putchar('\n');
