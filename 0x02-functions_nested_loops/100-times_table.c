8263c4b03321cbe734e0974c4540b2925b061213
KaphAein <cherifafadaly@gmail.com>
2023-08-15 16:58:52 -0700

    overwhelming

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_times_table - function that prints multiplications tables.
+ *@n: integer to be analyzed.
+ *
+ */
+
+void print_times_table(int n);
+{
+	int i, j, mult;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				mult = i * j;
+				if (mult < 10)
+					_putchar(' ');
+				if else (mult > 10 && mult < 100)
+							_putchar(mult / 10 + '0');
+				if else (mult > 10 && mult < 100)
+						{
+						_putchar(mult / 100 + '0');
+						_putchar(mult / 10 + '0');
+						}
+				_putchar(mult % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
