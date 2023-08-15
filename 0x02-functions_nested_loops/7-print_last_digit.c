297bd4296809de890429d822e2e499c2c213d490
KaphAein <cherifafadaly@gmail.com>
2023-08-15 09:36:23 -0700

    8 nes

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  print_last_digit - function that prints last digit.
+ * @num: integer to be checked.
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int num);
+{
+	int res = num%10;
+	return (res);
+}
