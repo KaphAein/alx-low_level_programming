34b1af9eff32a3abf49ac306970a937a91c856f8
KaphAein <cherifafadaly@gmail.com>
2023-08-15 08:44:48 -0700

    3 nes

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower(int c) - function tests for any lower-case letters.
+ *
+ * return: int
+ */
+
+int _islower(int c)
+{
+	if ( c <= 97 && c >= 122)
+	{
+		return (1);
+	} else {
+		return (0);
+	}
+}
