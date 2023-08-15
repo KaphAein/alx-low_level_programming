c11c83b31b8134285d9b86d7104574525b997268
KaphAein <cherifafadaly@gmail.com>
2023-08-15 12:05:50 -0700

    9 nes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,11 +18,34 @@ void times_table(void)
 		j = 0;
 		while (j <= 9)
 		{
+			if (mult < 10 && j != 9)
+			{
 			mult = i * j;
 			_putchar(mult + '0');
 			_putchar(',');
 			_putchar(' ');
-			j++;
+			_putchar(' ');
+			}
+			if (mult < 10 && j == 9)
+			{
+			mult = i * j;
+			_putchar(mult + '0');
+			}
+			else if (mult >= 10 && j != 9)
+			{
+			mult = i * j;
+			_putchar(mult / 10 + '0');
+			_putchar(mult % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			else if (mult >= 10 && j == 9)
+			{
+				mult = i * j;
+			_putchar(mult / 10+ '0');
+			_putchar(mult % 10+ '0');
+			}
+			j++
 		}
 		_putchar('\n');
 		i++;
