acf88d862fdbbc49a467f72bdf5d5caf7cd2ec48
KaphAein <cherifafadaly@gmail.com>
2023-08-16 08:08:17 -0700

    1 debbug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int r = positive_or_negative(0);
+	printf("%d", r);
+	return (0);
+}
