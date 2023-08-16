f3ee531378bf5ce731e5b1c97bb96d0c30d0b54b
KaphAein <cherifafadaly@gmail.com>
2023-08-16 05:09:51 -0700

    new day

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,14 +9,12 @@
 int main(void)
 {
 	int sum = 0;
-	int n = 0:
+	int n = 0;
 
 	for (n; n < 1024; n++)
 	{
 		if (n % 3 == 0 || n % 5 == 0)
-		{
 			sum = sum + n;
-		}
 	}
 	printf ("sum of natural numbers below 1024 = %d", sum);
     return (0);
