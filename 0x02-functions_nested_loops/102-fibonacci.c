c9b56350ad44abc516e8fb67d4e371c8b987d0f0
KaphAein <cherifafadaly@gmail.com>
2023-08-16 06:04:52 -0700

    new day

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, a, b, sum;
+
+	a = 1;
+	b = 2;
+	printf("%d, %d, ", a, b);
+	for (i = 0, i < 48, i++)
+	{
+		sum = a + b;
+		printf(", ");
+		printf("%d", sum);
+
+		a = b;
+		b = sum;
+	}
+	printf("\n");
+	return (0);
+}
