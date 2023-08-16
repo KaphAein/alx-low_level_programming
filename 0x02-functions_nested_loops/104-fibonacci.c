9e0a8c08ec5003149688a925307f11330a0f207c
KaphAein <cherifafadaly@gmail.com>
2023-08-16 07:08:56 -0700

    last dayz

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,19 +9,41 @@
 int main(void)
 {
 	int i;
-	unsigned long a, b, sum;
+	unsigned long a, b, a1, a2, b1, b2, sum, sum1, sum2;
+	unsigned long l = 1000000000;
 
 	a = 1;
 	b = 2;
 	printf("%ld, %ld", a, b);
 	for (i = 0; i < 96; i++)
 	{
+
+		if (i <= 88)
+		{
 		sum = a + b;
 		printf(", ");
 		printf("%ld", sum);
-
 		a = b;
 		b = sum;
+		}
+
+		a1 = (a / l);
+		a2 = (a % l);
+		b1 = (b / l);
+		b2 = (b % l);
+		sum1 = (sum / l);
+		sum2 = (sum % l);
+
+		if (i > 88)
+		{
+			printf(", ");
+			printf("%ld", sum1 + (sum2 / l));
+			printf("%ld", sum2 % l);
+			a1 = b1;
+			a2 = b2;
+			b1 = sum1;
+			b2 = sum2;
+		}
 	}
 	printf("\n");
 	return (0);
