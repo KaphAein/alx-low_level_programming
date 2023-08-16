f4847caf177c1c123443bc38fdabd40a80377e00
KaphAein <cherifafadaly@gmail.com>
2023-08-16 06:20:30 -0700

    new dayz

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long a, b, sum, sumofsums;
+
+	a = 1;
+	b = 2;
+	for (i = 0; i < 48; i++)
+	{
+		sum = a + b;
+		if (sum % 2 == 0 && sum < 4000000)
+		{
+			sumofsums += sum;
+		}
+		a = b;
+		b = sum;
+	}
+	printf("%ld", sumofsums);
+	printf("\n");
+	return (0);
+}
