d91e19e2682138f3d28279cc25ff99da083cf7a2
KaphAein <cherifafadaly@gmail.com>
2023-08-17 10:03:16 -0700

    100 more

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long long a, n, d;
+
+	a = 612852475143;
+	f = 2;
+	for (f; a / f > 1; f++)
+	{
+		if (a % f == 0)
+		{
+			a = a / f;
+		}
+		else
+			continue;
+	}
+	printf("%llu\n", a);
+	return (0);
+}
