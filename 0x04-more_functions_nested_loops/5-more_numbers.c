46f8ec1b1ac8cda4a4705caf1788344893a9c707
KaphAein <cherifafadaly@gmail.com>
2023-08-17 06:15:16 -0700

    5 more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers (0-9).
+ *
+ * Return: void.
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+			_putchar(j + '0');
+		_putchar('\n');
+	}
+}
