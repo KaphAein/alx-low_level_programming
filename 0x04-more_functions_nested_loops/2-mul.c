b631eb5b8444a50600ed1d7d0f48bb502125f773
KaphAein <cherifafadaly@gmail.com>
2023-08-17 05:55:04 -0700

    2 more

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,39 +4,13 @@
  * mul -  multiplies 2 numbers.
  * @a: number to be mult.
  * @b: number to be mult.
- * Return: 0 success.
+ * Return: mult.
  */
 
 int mul(int a, int b)
 {
-	int mult, divisor, digit;
+	int mult;
 
 	mult = a * b;
-	divisor = 1;
-
-	if (mult >= 0)
-	{
-		while ((mult / divisor) >= 10)
-			divisor *= 10;
-		while (divisor > 0)
-		{
-			digit = (mult / divisor) % 10;
-			putchar(digit + '0');
-			divisor /= 10;
-		}
-	}
-	if (mult < 0)
-	{
-		mult = -mult;
-		_putchar('-');
-		while ((mult / divisor) >= 10)
-			divisor *= 10;
-		while (divisor > 0)
-		{
-			digit = (mult / divisor) % 10;
-			putchar(digit + '0');
-			divisor /= 10;
-		}
-	}
-	_puthcar('\n');
+	return (mult);
 }
