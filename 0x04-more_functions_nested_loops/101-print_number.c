d01ddae6b99acda8c5313929a4812caf2136190f
KaphAein <cherifafadaly@gmail.com>
2023-08-17 10:37:37 -0700

    101 more

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_number -  prints number
+ * @n: number to be printed.
+ *
+ * Return: void.
+ */
+
+void print_number(int n)
+{
+	int divisor, digit;
+
+	divisor = 1;
+
+	if (n >= 0)
+	{
+		while ((n / divisor) >= 10)
+			divisor *= 10;
+		while (divisor > 0)
+		{
+			digit = (n / divisor) % 10;
+			putchar(digit + '0');
+			divisor /= 10;
+		}
+	}
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+		while ((n / divisor) >= 10)
+			divisor *= 10;
+		while (divisor > 0)
+		{
+			digit = (mult / divisor) % 10;
+			putchar(digit + '0');
+			divisor /= 10;
+		}
+	}
+	_puthcar('\n');
+}
