7ef95aeced257bb8bbc1c9a8e34dfdb9f1b5a777
KaphAein <cherifafadaly@gmail.com>
2023-08-21 02:15:20 -0700

    poi

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* puts_half - function that takes a pointer
+* @str: pointer
+* Return: void
+*/
+
+void puts_half(char *str);
+{
+	int i, size;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	size = --i;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
