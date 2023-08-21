ad07bb6e3c3d44993d75eafa1693766e9fb25e28
KaphAein <cherifafadaly@gmail.com>
2023-08-21 01:06:22 -0700

    1 poi

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _print_rev - function that takes a pointer
+* @s: pointer
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
