3c73a4fc638db0c297638921d3639196dc4513e2
KaphAein <cherifafadaly@gmail.com>
2023-08-21 03:44:53 -0700

    poi

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strcpy - function that takes a pointer
+* @dest: pointer
+* @src: pointer
+* return: char
+*/
+
+char *_strcpy(char *dest, char *src);
+{
+	int i;
+
+	for ( i = 0; i >= 0; i++ )
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	return (*dest);
+}
