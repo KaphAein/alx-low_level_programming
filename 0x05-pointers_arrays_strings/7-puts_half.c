adb94b88b5b861ba73dcc1462c7258b29ddc2ea3
KaphAein <cherifafadaly@gmail.com>
2023-08-21 02:30:09 -0700

    poi

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,22 +6,26 @@
 * Return: void
 */
 
-void puts_half(char *str);
+void puts_half(char *str)
 {
-	int i, size;
+	int i, size, hfsz;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
 		i++;
 	}
 
 	size = --i;
+	if (size % 2 == 0)
+		hfsz = size / 2;
+	else if (size % 2 != 0)
+		hfsz = (size - 1) /2;
 	i = 0;
-	while (str[i] != '\0')
+	while (str[hfsz] != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(str[hfsz]);
+		hfsz++;
 	}
 	_putchar('\n');
 }
