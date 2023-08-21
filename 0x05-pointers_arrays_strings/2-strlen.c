1c5d39f1c5459e049e9e3e74b4654c1a156421e1
KaphAein <cherifafadaly@gmail.com>
2023-08-21 00:48:32 -0700

    1 poi

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
 * _strlen - function that takes a pointer
 * @s: pointer
@@ -9,5 +9,12 @@
 
 int _strlen(char *s)
 {
-	return strlen(s);
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
