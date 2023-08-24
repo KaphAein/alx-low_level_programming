10523125e4933993248eddc9366c6d755d5f39a3
KaphAein <cherifafadaly@gmail.com>
2023-08-24 05:30:05 -0700

    \more pointers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * infinite_add - function that adds two numbers
+ * @n1: number 1
+ * @n2: number 2
+ * @r: buffer that the function will use to store the result
+ * @size_r: is the buffer size
+ * Return: pointer to the result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, k, num1, num2, sum, carry = 0;
+
+	for (i = 0; n1[i] != '\0'; i++);
+	for (j = 0; n2[j] != '\0'; j++);
+
+	if (size_r <= i || size_r <= j)
+		return (0);
+
+	r(size_r - 1) = '\0';
+	i--;
+	j--;
+	k = size_r - 2;
+	while (i >= 0 || j >= 0 || carry > 0)
+	{
+
+		if (i >= 0) {
+			num1 = n1[i] - '0';
+		}
+		else
+		{
+			num1 = 0;
+		}
+
+		if (j >= 0) {
+			num2 = n2[j] - '0';
+		}
+		else
+		{
+			num2 = 0;
+		}
+
+		sum = num1 + num2 + carry;
+
+		r[k] = (sum % 10) + '0';
+
+		carry = sum / 10;
+
+
+		i--;
+		j--;
+		k--;
+	}
+
+	if (k < 0)
+		return (0);
+
+	return &r[k + 1];
+}
