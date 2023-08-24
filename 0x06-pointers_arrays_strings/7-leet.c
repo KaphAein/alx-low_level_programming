83ddc73d41db7361dbbd231de92eaf522766df7f
KaphAein <cherifafadaly@gmail.com>
2023-08-24 03:05:51 -0700

    \more pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,7 +14,7 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; aeotl[j] || AEOTL[j]; j++)
+		for (j = 0; j < 5; j++)
 		{
 			if (s[(i)] == aeotl[j] || s[(i)] == AEOTL[j])
 				s[i] = leet[j];
