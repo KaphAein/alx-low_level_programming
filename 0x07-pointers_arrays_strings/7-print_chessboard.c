542f1c76fb3c4c23059cf22f79617744dc4112b3
KaphAein <cherifafadaly@gmail.com>
2023-08-28 06:03:10 -0700

    \even more poi

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_chessboard - function that takes elements
+ * @a: element
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		_putchar ("a[i]");
+		i++;
+}
