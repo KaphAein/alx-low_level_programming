da92c82c4538a94ad27657bcd462a934732f8e84
KaphAein <cherifafadaly@gmail.com>
2023-08-28 05:39:25 -0700

    \even more poi

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - function that takes elements
+ * @haystack: element
+ * @needle: element
+ * Return: cahr
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*haystack != '\0')
+	{
+		i = 0;
+		while (*needle == *haystack && *needle != '\0')
+		{
+			haystack++, needle++, i++;
+			if (needle == '\0')
+				return (haystack - i);
+			haystack -= (i - 1);
+			needle -= i;
+		}
+	}
+	return ('\0');
+}
