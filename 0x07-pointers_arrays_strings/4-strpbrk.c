d0611f940e52ff1928f95171d1240a15c4bc2fd0
KaphAein <cherifafadaly@gmail.com>
2023-08-28 04:42:32 -0700

    \even more poi

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that takes elements
+ * @s: element
+ * @accept: element
+ * Return: cahr
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j, n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+	}
+	return (s[i] == accept ? (s + i) : ('\0'));
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c~
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - function that takes elements
+ * @s: element
+ * @accept: element
+ * Return: unsigned int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, n = 0;
+
+	for (i = 0; s[i] != ' '; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				n += 1;
+				break;
+			}
+		}
+	}
+	return (n);
+}
