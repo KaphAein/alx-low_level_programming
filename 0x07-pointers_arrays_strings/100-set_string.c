5248ebb5d472184766da1d7eba9b9fea7e2b69be
KaphAein <cherifafadaly@gmail.com>
2023-08-28 07:16:01 -0700

    \even more poi

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function that takes elements
+ * @a: element
+ * @to: element
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
