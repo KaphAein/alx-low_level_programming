7e120af7337aad045a398893e5f1a63380043ec2
KaphAein <cherifafadaly@gmail.com>
2023-08-28 04:53:25 -0700

    \even more poi

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,10 +16,9 @@ char *_strpbrk(char *s, char *accept)
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-			{
 				return (s + i);
-			}
+			else
+				return ('\0');
 		}
 	}
-	return ('\0');
 }
