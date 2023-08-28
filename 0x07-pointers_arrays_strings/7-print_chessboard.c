d2871ba59349f3d1bd387be852051fcf63faff00
KaphAein <cherifafadaly@gmail.com>
2023-08-28 06:12:02 -0700

    \even more poi

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,10 +9,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i = 0;
+	int i, j;
 
-	while (a[i] != '\0')
+	for (i = 0; i < 8; i++)
 	{
-		_putchar ("a[i]");
-		i++;
+		for (j = 0; j < 8; j++)
+			_putchar ("a[i][j]");
+		_putchar ("\n");
+	}
 }
