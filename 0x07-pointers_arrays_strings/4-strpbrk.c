b4e5b6903e41404597fda75cd22e7b97ff8d67e7
KaphAein <cherifafadaly@gmail.com>
2023-08-28 04:56:35 -0700

    \even more poi

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,7 +9,7 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j, n = 0;
+	unsigned int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -17,8 +17,7 @@ char *_strpbrk(char *s, char *accept)
 		{
 			if (s[i] == accept[j])
 				return (s + i);
-			else
-				return ('\0');
 		}
 	}
+	return ('\0');
 }
