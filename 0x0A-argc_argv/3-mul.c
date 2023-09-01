225eddfec3708793ec7b9d7ddab62ef20d98b4b1
KaphAein <cherifafadaly@gmail.com>
2023-09-01 08:43:22 -0700

    \stat lib

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: element
+ * @argv: element
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+		printf("%i\n", (int)(argv[1] * argv[2]));
+	else if (argc < 3)
+		printf("Error");
+	}
+	return (0);
+}
