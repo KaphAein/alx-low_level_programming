161f1e777f539bb5b9bed447e4207d4cad391727
KaphAein <cherifafadaly@gmail.com>
2023-09-01 10:04:58 -0700

    \stat lib

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,54 +3,40 @@
 #include <ctype.h>
 
 /**
- * _isnumber - function
- * @s: element
- * Return: On success 1.
- */
-
-int _isnumber(char *s)
-{
-	int i = 0, r = 1, d = 0;
-
-	if (*s == '-')
-		i++;
-	for (; *(s + i) != 0; i++)
-	{
-		d = isdigit(*(s + i));
-		if (d == 0)
-		{
-			r = 0;
-			break;
-		}
-	}
-	return (r);
-}
-
-/**
- * main - Entry point
+ * main - entry point
  * @argc: element
  * @argv: element
- * Return: Always 0 (Success)
+ * Return: 1 on success
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, sum  = 0, r = 0;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			if (_isnumber(argv[i]))
-				sum += atoi(argv[i]);
-			else
-				r = 1;
-		}
-	}
 	if (argc < 2)
 		printf("0\n");
-	else if (!_isnumber(argv[i]))
-		printf("Error\n");
+	else
+	{
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
 
-	return (r);
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+	printf("%d\n", sum);
+	}
+	return (0);
 }
