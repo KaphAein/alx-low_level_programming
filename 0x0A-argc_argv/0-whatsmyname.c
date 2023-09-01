4efc34547b7bcee6d5582c76c7b77ae4d3307a50
KaphAein <cherifafadaly@gmail.com>
2023-09-01 08:23:26 -0700

    \stat lib

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,14 @@
 
 /**
  * main - Entry point
- *
+ * @argc: element
+ * @argv: element
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int main(int argc, char **argv)
 {
-	printf("%s\n", argv[0]);
+	if(argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: element
+ * @argv: element
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	if(argc >= 0)
+		printf("%d\n", arg);
+	return (0);
+}
