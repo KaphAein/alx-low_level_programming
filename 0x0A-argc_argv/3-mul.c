f2bf73cb3bbac9a46573562d2fb75b4e7f8a5c98
KaphAein <cherifafadaly@gmail.com>
2023-09-01 08:50:59 -0700

    \stat lib

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,9 +11,11 @@
 int main(int argc, char **argv)
 {
 	if (argc == 3)
-		printf("%i\n", (int) (argv[1] * argv[2]));
-	else if (argc < 3)
-		printf("Error");
-
+		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 	return (0);
 }
