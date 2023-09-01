8835da8ddad591905710bb97a75ae213fa254d8d
KaphAein <cherifafadaly@gmail.com>
2023-09-01 09:26:25 -0700

    \stat lib

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,20 +12,22 @@ int main(int argc, char **argv)
 {
 	int i = 1, sum = 0;
 
-	while (i < argc - 1)
+	if (**argv > '9' || **argv < '0')
 	{
-		if (**argv > '9' || **argv < '0')
-		{
-			printf("Error\n");
-			return (1);
-		}
-		if (argc == 1)
-			printf("0\n");
-		else if (argc > 1)
+		printf("Error\n");
+		return (1);
+	}
+
+	if (argc == 1)
+		printf("0\n");
+
+	else if (argc > 1)
+	{
+		while (i < argc - 1)
 		{
 			sum += atoi(argv[i]);
+			i++;
 		}
-		i++;
 	}
 	printf("%i\n", sum);
 
