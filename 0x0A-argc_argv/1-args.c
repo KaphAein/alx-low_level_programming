8f7f1e75646de33bbe80aaedbe2ca4078a24f8f1
KaphAein <cherifafadaly@gmail.com>
2023-09-01 08:34:06 -0700

    \stat lib

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -10,6 +11,6 @@
 int main(int argc, char **argv)
 {
 	(void) argv;
-	printf("%d\n", argc);
+	printf("%i\n", argc - 1);
 	return (0);
 }
