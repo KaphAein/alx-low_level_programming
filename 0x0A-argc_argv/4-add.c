fe619ceec47e9f3535a6f8ea8f43cab99464bd7d
KaphAein <cherifafadaly@gmail.com>
2023-09-01 09:04:02 -0700

    \stat lib

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: element
+ * @argv: element
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 1, sum = 0;
+
+	while (i < argc - 1)
+	{
+		if (*argv > 57 || *argv < 48)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else if (argc == 0)
+			printf("0\n");
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%i\n", sum);
+
+	return (0);
+}
