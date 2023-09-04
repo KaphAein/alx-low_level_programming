97bb05b0959d869b4f8041e0e4a0a2a6bb25383c
KaphAein <cherifafadaly@gmail.com>
2023-09-04 07:25:00 -0700

    \malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,16 +1,17 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * *create_array - function
  * @size: element
  * @c: element
- * return: char or NULL
+ * Return: char or NULL
  */
 
 char *create_array(unsigned int size, char c)
 {
 	char *a;
-	int i;
+	unsigned int i;
 
 	if (size <= 0)
 		return (NULL);
