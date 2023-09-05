03e0cea9f3ed22d8b87830da5fcecb18cd0efca0
KaphAein <cherifafadaly@gmail.com>
2023-09-05 11:54:23 -0700

    \malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,56 +1,84 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * **strtow - function
- * @str: element
- * Return: char
+ * _wcount - counts number of words
+ * @sw: string
+ *
+ * Return: int
+ */
+int _wcount(char *swc)
+{
+	int l, wc;
+
+	i = 0, wc = 0;
+	if (swc[i] == ' ')
+		i++;
+	while (swc[i])
+	{
+		if (swc[i] == ' ' && swc[i + 1] != ' ')
+			wc++;
+		if (swc[i] != ' '  && swc[i + 1] == 0)
+			wc++;
+		i++;
+	}
+	return (wc);
+}
+/**
+ * _trspace - Moves adress to remove trailig whitespaces
+ * @st: string
+ *
+ * Return: Pointer
+ */
+char *_trspace(char *st)
+{
+	while (*st == ' ')
+		st++;
+	return (st);
+}
+/**
+ * strtow - splits a string into words
+ * @str: string
+ *
+ * Return: Double Pointer
  */
 char **strtow(char *str)
 {
-	int i, j, k, wc = 0, lc = 0, wl = 0;
-	char **a;
+	char **s, *ts;
+	int l, l2, wc, i, j, fr, k;
 
-	if (str == "" || str == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		while (str[i] == " ")
-			i++;
-		if (str[i] != " " && (str[i + 1] == " " || str[i + 1] == '\0'))
-			wc += 1;
-		lc++;
-	}
+	if (str == NULL || *str == 0)
+		return (0);
+	fr = 0;
+	wc = _wcount(str);
 	if (wc == 0)
-		return (NULL);
-	a = malloc(sizeof(char) * (lc + wc + 1));
-	if (a == NULL)
-		return (NULL);
-	j = 0;
-	for (i = 0; i < lc; i++)
+		return (0);
+	s = malloc((wc + 1) * sizeof(char *));
+	if (s == 0)
+		return (0);
+	ts = _trspace(str);
+	for (i = 0; i < wc; i++)
 	{
-		while (str[i] == " ")
-			i++;
-		wl = 0;
-		while (str[i + wl] != " " && str[i + wl] != '\0')
-			wl++;
-		if (wl > 0)
+		l = 0;
+		while (*(ts + l) != ' ' && *(ts + l) != 0)
+			l++;
+		s[i] = malloc((l + 1) * sizeof(char));
+		if (s[i] == 0)
 		{
-			a[j] = malloc(sizeof(char) * (wl + 1));
-			if (a[j] == NULL)
-			{
-				for (k = 0; k < j; k++)
-					free(a[k]);
-				free(a);
-				return (NULL);
-			}
-			k = 0;
-			while (k < wl)
-				a[j][k] =  str[i];
-			a[j][k] = '\0';
-			j++;
-			i += wl;
+			fr = 1;
+			break;
 		}
+		for (j = 0, l2 = 0; l2 < l; l2++, j++)
+			s[i][j] = *(ts + l2);
+		s[i][j] = '\0';
+		ts = _trspace(ts + l);
 	}
-	a[j] = '\0';
-	return (a);
+	s[i] = NULL;
+	if (fr == 1)
+	{
+		for (k = 0; k <= i; k++)
+			free(s[k]);
+		free(s);
+	}
+	return (s);
 }
