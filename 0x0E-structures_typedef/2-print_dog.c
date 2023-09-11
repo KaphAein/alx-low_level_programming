619dd8e90f4b5bc443ac0c2570797a3066917574
KaphAein <cherifafadaly@gmail.com>
2023-09-11 07:07:50 -0700

    “strsysdef”

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,19 +10,10 @@
 
 void print_dog(struct dog *d)
 {
-	d = malloc(sixeof(struct dog));
 	if (d == NULL)
 		return;
-	if (d->name == NULL)
-		printf("Name: (nil)");
-	if (d->age == NULL)
-		printf("Age: (nil)");
-	if (d->owner == NULL)
-		printf("Owner: (nil)");
-	else
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %d\n", d->age);
-		printf("Owner: %s\n" d->owner);
-	}
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+
 }
