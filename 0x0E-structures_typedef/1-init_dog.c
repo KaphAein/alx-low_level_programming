075fb649c9082bcec7d288c014e703109a238571
KaphAein <cherifafadaly@gmail.com>
2023-09-11 06:01:18 -0700

    “strsysdef”

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,19 +1,20 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
  * init_dog - initializes a variable of type struct dog
  * @d: element
  * @name: element
  * @age: element
  * @owner: element
- * return: void
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = malloc(sizeof(struct dog));
 	if (d == NULL)
-		return (NULL);
+		return;
 
 	d.name = name;
 	d.age = age;
