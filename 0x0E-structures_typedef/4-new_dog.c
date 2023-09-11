21dcf450ad32f99388fc959cc67c7b16e2f5af88
KaphAein <cherifafadaly@gmail.com>
2023-09-11 10:01:01 -0700

    “strsysdef”

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,35 @@
 #include <stdio.h>
 
 /**
+ * *_strdup - function
+ * @str: element
+ * Return: char or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	unsigned int i = 0, size;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+
+	size = i + 1;
+
+	a = malloc(sizeof(char) * size);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		a[i] = str[i];
+	a[i] = '\0';
+	return (a);
+}
+
+/**
  * new_dog - function that prints a struct dog
  * @name: element
  * @age: element
@@ -17,17 +46,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = strdup(name);
+	d->name = _strdup(name);
 	if (d->name == NULL)
 	{
-		free(d->name);
+		free(d);
 		return (NULL);
 	}
 	d->age = age;
-	d->owner = strdup(owner);
+	d->owner = _strdup(owner);
 	if (d->owner == NULL)
 	{
-		free(d->owner);
+		free(d-name);
+		free(d);
 		return (NULL);
 	}
 	return (d);
