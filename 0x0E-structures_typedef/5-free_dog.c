e418b395702b0043dfaceb18f07adfa7052b92a7
KaphAein <cherifafadaly@gmail.com>
2023-09-11 07:54:10 -0700

    “strsysdef”

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog - function that prints a struct dog
+ * @name: element
+ * @age: element
+ * @owner: element
+ * Return: NULL if function fail
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}
