4b2347f66545597475080fd1db4904fcd46dc181
KaphAein <cherifafadaly@gmail.com>
2023-09-11 07:47:56 -0700

    “strsysdef”

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,11 +12,12 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	new_dog = malloc(sizeof(dog_t))
-	if (new_dog == NULL)
-		return(NULL);
+	dog_t *d;
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return (new_dog);
+	return (d);
 }
