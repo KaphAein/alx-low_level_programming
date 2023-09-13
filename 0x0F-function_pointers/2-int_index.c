7f0e89201a7e37150612f7e0e91b1e8d8789eccb
KaphAein <cherifafadaly@gmail.com>
2023-09-13 04:58:16 -0700

    “funptr”

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer
+ * @array: element
+ * @size: element
+ * @cmp: function
+ * Return: void
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int *ptr;
+
+	if (int_index == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i])
+			break;
+	return (i);
+}
