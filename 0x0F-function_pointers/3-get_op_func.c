8a1f4729c7dae0635d3bd797185b6da94c410940
KaphAein <cherifafadaly@gmail.com>
2023-09-13 08:30:05 -0700

    “funptr”

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include "3-calc.h"
+#include <string.h>
 /**
  * get_op_func - selects the correct function
  * @s: operator
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,35 +1,41 @@
 #include "function_pointers.h"
 #include "3-calc.h"
+#include <string.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(argc, argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	char *operator = "+-*/%";
+	char *operator;
 
+	operator = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	if (argc != 4)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2] != operator)
+	if (strcmp(operator, "+") != 0 &&
+		strcmp(operator, "-") != 0 &&
+		strcmp(operator, "*") != 0 &&
+		strcmp(operator, "/") != 0 &&
+		strcmp(operator, "%") != 0)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2] == '/' || argv[2] == '%') && argv[3] == 0)
+	if ((strcmp(operator, "/") || strcmp(operator, "%")) && num2 == 0)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(100);
 	}
 
-	num1 = int atoi(const char argv[1]);
-	num2 = int atoi(const char argv[3]);
-
-	result = (*get_op_func(operator))(num1, num2)
+	result = (*get_op_func(operator))(num1, num2);
    	printf("%d\n", result);
 
     return (0);
