124f50c4441af0d1ee4ecb346b0a252ba62130fe
KaphAein <cherifafadaly@gmail.com>
2023-09-13 05:00:46 -0700

    “funptr”

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,12 +11,13 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	int *ptr;
 
 	if (int_index == NULL || size <= 0)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]))
 			break;
+
 	return (i);
 }
