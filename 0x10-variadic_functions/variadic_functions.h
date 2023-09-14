33f50774ff7b2f73f274302f6c80e6839cd31e3d
KaphAein <cherifafadaly@gmail.com>
2023-09-14 05:22:21 -0700

    “varfun”

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VAR_FUN_C
+#define VAR_FUN_C
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+
+#endif
