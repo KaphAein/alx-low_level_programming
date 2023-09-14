2474aabbeb26fcbd9934c93e143382aedcd62bb0
KaphAein <cherifafadaly@gmail.com>
2023-09-14 06:09:46 -0700

    “varfun”

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,15 +14,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	char *str;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(args, char *) == NULL)
+		char *str = va_arg(args, char *);
+		if (str == NULL)
 			printf("(nil)");
 		else
-			printf("%s", va_arg(args, char *));
+			printf("%s", str);
 		if (separator && i != (n - 1))
 			printf("%s", separator);
 	}
