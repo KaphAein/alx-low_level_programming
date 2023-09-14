6882edc99fc08be4de120e1f67c69b7bf2b07810
KaphAein <cherifafadaly@gmail.com>
2023-09-14 05:56:36 -0700

    “varfun”

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings, followed by a new line.
+ * @n: number of args
+ * @separator: string
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(args, char *));
+		if (separator && i != (n - 1))
+			printf("%s", separator);
+		if (separator == NULL && i != (n - 1))
+			printf("(nil)");
+	}
+	printf("\n");
+	va_end(args);
+}
