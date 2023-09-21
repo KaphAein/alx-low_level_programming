9232b4cc80bb14821f6cc5d874050155d2d09f1f
kaphaein <cheriffadaly@github.com>
2023-09-21 11:04:45 -0700

    “linkedlist”

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,12 +19,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-
 	new->next = *head;
 	*head = new;
 
