7a0f223f9e69048a5c8230c5112cbc74ed788a93
KaphAein <cherifafadaly@gmail.com>
2023-09-21 07:23:52 -0700

    “linkedlist”

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list.
+ * @h: list to  be handled by function
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+			break;
+		}
+		printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h->next;
+	}
+	return (count);
+}
