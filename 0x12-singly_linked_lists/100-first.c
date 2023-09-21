0c59555ab8a01c5d7fd8c7f0b24285b5bdd15c21
kaphaein <cheriffadaly@github.com>
2023-09-21 11:36:43 -0700

    “linkedlist”

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * before_main - function executed before main
+ * Return: void.
+ */
+
+void __attribute__ ((constructor)) freebie(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
