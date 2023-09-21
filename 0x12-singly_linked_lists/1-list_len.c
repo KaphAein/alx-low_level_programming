4b1303109fd0e33e60dcb2873ac5aecc47c01152
kaphaein <cheriffadaly@github.com>
2023-09-21 10:04:49 -0700

    “linkedlist”

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - function returns number of elements
+ * @h: list to  be handled by function
+ * Return: number of elments in linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		size_t count;
+
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
