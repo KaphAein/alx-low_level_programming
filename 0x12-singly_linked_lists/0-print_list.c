75022d2a294da0654c4c92ff6850420163396eef
kaphaein <cheriffadaly@github.com>
2023-09-21 09:48:19 -0700

    “linkedlist”

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,11 +13,9 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)");
-			break;
-		}
-		printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		count++;
 		h = h->next;
 	}
