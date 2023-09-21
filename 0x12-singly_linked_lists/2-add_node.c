c831ddae8bbb8f3f24cea281a8993004e28ada2b
kaphaein <cheriffadaly@github.com>
2023-09-21 10:57:39 -0700

    “linkedlist”

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node
+ * @h: list to  be handled by function
+ * Return: number adress of new node or NULL
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return(NULL);
+	}
+
+	new->next = head;
+	*head = new;
+
+	return (new);
+}
