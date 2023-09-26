85483f11bdfcafa67ae7ecedb0ff7f7d200dcde3
kaphaein <cheriffadaly@github.com>
2023-09-26 10:00:55 -0700

    “morelinkedlist”

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that deletes the head node
+ * @head: list to be handled by function
+ * Return: void
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *current;
+	int value;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	current = *head;
+	value = current->n;
+	*head  = current->next;
+	current = NULL;
+	return (value);
+}
