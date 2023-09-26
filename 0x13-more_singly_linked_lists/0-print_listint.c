39a9a5d1feb13c00c7d8cd30503e7bde6ba3547c
kaphaein <cheriffadaly@github.com>
2023-09-26 04:16:45 -0700

    “morelinkedlist”

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_lisintt - function that prints all the elements of a list.
+ * @h: list to  be handled by function
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
