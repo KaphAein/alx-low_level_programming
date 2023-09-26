b73d550759edcea225e6fed63343aeab6ae3b21c
kaphaein <cheriffadaly@github.com>
2023-09-26 12:46:07 -0700

    “morelinkedlist”

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	if (!idx)
+	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
@@ -37,7 +37,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (new);
 		}
 		else
+		{
 			temp = temp->next;
+			i++;
+		}
 	}
 
 	return (NULL);
