d74974104bde86b96a9d1d67beff5a9b2a4beb37
kaphaein <cheriffadaly@github.com>
2023-09-26 12:39:05 -0700

    “morelinkedlist”

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - function that inserts a new node
+ * @n: element int
+ * @head: first element of linked list
+ * @idx: index
+ * Return: adress for the new element
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp;
+	listint_t *new;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL || !head)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (!idx)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	while (temp && i < idx)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+
+	return (NULL);
+}
+}
