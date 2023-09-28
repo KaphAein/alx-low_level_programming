a3fb084db8667e6eee49dd1acf01bdd8a33b2c85
kaphaein <cheriffadaly@github.com>
2023-09-28 05:12:35 -0700

    “bitman”

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i = 0, len, bin;
 
 	if ((*b != '1' && *b != '0') || b == NULL)
-			return (0);
+		return (0);
+
 	for (len = 0; b[len] != '\0'; len++)
 		;
 	i = 0;
