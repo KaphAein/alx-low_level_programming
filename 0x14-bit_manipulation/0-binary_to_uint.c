8f7bbafdb79f975abc6d383350b93bf131ce1836
kaphaein <cheriffadaly@github.com>
2023-09-28 05:11:11 -0700

    “bitman”

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary number
+ * @b: pointer to a string
+ * Return: converted number, or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0, len, bin;
+
+	if ((*b != '1' && *b != '0') || b == NULL)
+			return (0);
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	i = 0;
+	for (len--, bin = 1; len >= 0; len--, bin *= 2)
+	{
+		if (b[len] & 1)
+			i += bin;
+	}
+	return (i);
+}
