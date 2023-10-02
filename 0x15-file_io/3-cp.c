0f5ce1544c4e90cec96e46ce7d431d4bb60053d7
kaphaein <cheriffadaly@github.com>
2023-10-02 12:50:10 -0700

    “bitman”

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if files can be opened.
+ * error_file - function that checks if files can be opened.
  * @file_from: file_from.
  * @file_to: file_to.
- * @argv: arguments vector.
+ * @argv: the arguments vector.
  * Return: no return.
  */
 void error_file(int file_from, int file_to, char *argv[])
@@ -23,12 +23,12 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
+ * main - program that copies the content of a file to another file.
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 Always success
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int file_from, file_to, err_close;
 	ssize_t nchars, nwr;
