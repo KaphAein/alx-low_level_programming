51b25e51cde1f4bb7757f36b0a6dca4c124ac924
kaphaein <cheriffadaly@github.com>
2023-10-02 11:54:22 -0700

    “bitman”

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writen in file.
+ * Return: 1 if it success. -1 if it fails.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fw, len;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (-1);
+	for (len = 0; textcontent != '\0'; len++)
+		;
+	fw = write(fd, text_content, len);
+	if (fw == -1)
+		return(-1);
+	close(fd)
+		return(1);
+}
