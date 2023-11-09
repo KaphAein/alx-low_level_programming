b8b0201057d5ea5015d81dc35cb43dc2da742ff3
kaphaein <cheriffadaly@github.com>
2023-11-09 07:36:39 -0800

    “More”

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_dlistint - Frees dlinked list
+ * @head: List head
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	if(!head)
+	{
+		return;
+	}
+	current = head;
+	while (current)
+	{
+		free(current);
+		current = current->next;
+	}
+}
