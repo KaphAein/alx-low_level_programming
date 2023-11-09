7a50a44a494ee994ea8763203be481b712b0cc9c
kaphaein <cheriffadaly@github.com>
2023-11-09 08:44:45 -0800

    “More”

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-
+	if (idx == 0)
+		new = add_dnodeint(h, n);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index
+ * @head: List head
+ * @index: node index
+ * Return: return 1 for success -1 for fail
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int i;
+
+	if (head == NULL)
+		return (-1);
+
+	current = head;
+
+	for (i = 0; i < index && current; i++)
+		current = current->next;
+
+	if (!current)
+		return (-1);
+	if (current->prev)
+		current->prev->next = current->next;
+	else
+		*head = current->next;
+
+	if (current->next)
+		current->next->prev = current->prev;
+	free(current);
+	return (1);
+}
