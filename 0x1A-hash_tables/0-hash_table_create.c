378708a1877f74ce47ea4abec36ba6418e1f7a1c
kaphaein <cheriffadaly@github.com>
2023-12-01 05:34:33 -0800

    “HashTables”

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table.
+ * @size: the size of the array
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t newTable;
+	unsigned long int i;
+
+	newTable = malloc(sizeof(hash_table_t));
+	if (newTable == NULL)
+		return (NULL);
+	newTable->size = size;
+	newTable->array = malloc(sizeof(hash_node_t *) * size);
+	if (newTable->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		newTable->array[i] = NULL;
+	return newTable;
+}
