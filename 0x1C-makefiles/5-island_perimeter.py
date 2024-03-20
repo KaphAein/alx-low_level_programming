8fca8f62913390bd9770d906dfc66c40ca547472
kaphaein <cherifafadaly@gmail.com>
2024-03-20 13:32:45 -0700

    “MakeFile”

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,17 +8,16 @@ def island_perimeter(grid):
     height = len(grid)
     perim = 0
 
-    if width <= 100 and height <= 100:
-        for i in range(height):
-            for j in range(width):
-                if grid[i][j] == 1:
-                    perim += 4
-                    if grid[i][j + 1] == 1:
-                        perim -= 1
-                    if grid[i][j - 1] == 1:
-                        perim -= 1
-                    if grid[i + 1][j] == 1:
-                        perim -= 1
-                    if grid[i - 1][j] == 1:
-                        perim -= 1
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                perim += 4
+                if grid[i][j + 1] == 1:
+                    perim -= 1
+                if grid[i][j - 1] == 1:
+                    perim -= 1
+                if grid[i + 1][j] == 1:
+                    perim -= 1
+                if grid[i - 1][j] == 1:
+                    perim -= 1
     return perim
