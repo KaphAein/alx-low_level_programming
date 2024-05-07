6f123b1e13eb496abf013574ac455e9808fae5e0
KaphAein <cherifafadaly@gmail.com>
2024-05-07 14:44:22 -0700

    “SearchAlg”

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ * @array: input array
+ * @size: size of array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, high, low, mid;
+
+	if (array == 0)
+	{
+		return (-1);
+	}
+	low = 0;
+	high = size - 1;
+	while (low <= high)
+	{
+		mid = low + (high - low) / 2;
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i != high)
+				printf(", ");
+		}
+		if (value == array[mid])
+		{
+			return (mid);
+		}
+		if (value < array[mid])
+		{
+			high = mid - 1;
+		}
+		else
+		{
+			low = mid + 1;
+		}
+	}
+	return (-1);
+}
