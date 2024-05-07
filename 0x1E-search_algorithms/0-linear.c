1b79e74e8d2ab63c26d55043700943c73c9ce438
KaphAein <cherifafadaly@gmail.com>
2024-05-07 12:36:42 -0700

    “SearchAlg”

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
+ * @array: input array
+ * @size: size of array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array)
+	{
+		for (i = 0; i < (int)size; i++)
+		{
+			if (array[i] == value)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
